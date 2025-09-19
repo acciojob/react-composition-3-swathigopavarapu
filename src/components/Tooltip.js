af9cffd5718e3be2b466dff725af9c922b9bfca4
Swathi Gopavarapu <swathigopavarapu1@gmail.com>
2025-09-19 11:34:17 +0530

    initial commit

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -4,16 +4,17 @@ import "./../styles/App.css";
 const Tooltip = ({ text, children }) => {
   const [visible, setVisible] = useState(false);
 
-  return (
-    <div
-      className="tooltip"
-      onMouseEnter={() => setVisible(true)}
-      onMouseLeave={() => setVisible(false)}
-    >
-      {children}
-      {visible && <div className="tooltiptext">{text}</div>}
-    </div>
-  );
+  return React.cloneElement(children, {
+    className: (children.props.className || "") + " tooltip",
+    onMouseEnter: () => setVisible(true),
+    onMouseLeave: () => setVisible(false),
+    children: (
+      <>
+        {children.props.children}
+        {visible && <div className="tooltiptext">{text}</div>}
+      </>
+    )
+  });
 };
 
 export default Tooltip;
